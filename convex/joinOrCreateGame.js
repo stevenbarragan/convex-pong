4c73fd8df65d5c933787376713a171ff42e00b18
Steven Barragan <me@steven.mx>
2023-02-24 14:38:58 -0600

    [chore] fix tab in joinOrCreate file
    * and delete updateBall.js

--- convex/joinOrCreateGame.js
+++ convex/joinOrCreateGame.js
@@ -1,58 +1,58 @@
 import { mutation } from "./_generated/server";
 
 export default mutation(async ({ db }, boardSize, paddleSize) => {
-	var waiting = await db
-		.query('games')
-		.filter((q) => q.eq(q.field('status'), 'waiting'))
-		.first();
+  var waiting = await db
+    .query('games')
+    .filter((q) => q.eq(q.field('status'), 'waiting'))
+    .first();
 
-	if (waiting !== null) {
-		await db.patch(waiting._id, {status: 'ready'});
+  if (waiting !== null) {
+    await db.patch(waiting._id, {status: 'ready'});
 
-		return {
-			id: waiting._id,
-			player: 'right'
-		}
-	}
+    return {
+      id: waiting._id,
+      player: 'right'
+    }
+  }
 
-	const ballId = await db.insert("balls", {
-		direction: 0,
-		x: boardSize / 2,
-		y: boardSize / 2
-	});
+  const ballId = await db.insert("balls", {
+    direction: 0,
+    x: boardSize / 2,
+    y: boardSize / 2
+  });
 
-	const left = await db.insert("paddles", {
-		position: (boardSize - paddleSize) / 2
-	});
+  const left = await db.insert("paddles", {
+    position: (boardSize - paddleSize) / 2
+  });
 
-	const right = await db.insert("paddles", {
-		position: (boardSize - paddleSize) / 2
-	});
+  const right = await db.insert("paddles", {
+    position: (boardSize - paddleSize) / 2
+  });
 
-	const leftPing = await db.insert("pings", {
-		time: Math.floor(Date.now() / 1000)
-	});
+  const leftPing = await db.insert("pings", {
+    time: Math.floor(Date.now() / 1000)
+  });
 
-	const rightPing = await db.insert("pings", {
-		time: Math.floor(Date.now() / 1000)
-	});
+  const rightPing = await db.insert("pings", {
+    time: Math.floor(Date.now() / 1000)
+  });
 
-	const gameData = {
-		status: 'waiting',
-		countDown: 3,
-		ballId,
-		left,
-		right,
-		leftScore: 0,
-		rightScore: 0,
-		leftPing,
-		rightPing
-	};
+  const gameData = {
+    status: 'waiting',
+    countDown: 3,
+    ballId,
+    left,
+    right,
+    leftScore: 0,
+    rightScore: 0,
+    leftPing,
+    rightPing
+  };
 
-	const game = await db.insert("games", gameData);
+  const game = await db.insert("games", gameData);
 
-	return {
-		id: game,
-		player: 'left'
-	}
+  return {
+    id: game,
+    player: 'left'
+  }
 })
--- convex/updateBall.js
+++
@@ -1,5 +0,0 @@
-import { mutation } from "./_generated/server";
-
-export default mutation(async ({ db }, id, coordenates) => {
-	await db.patch(id, coordenates);
-})
